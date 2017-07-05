f5c45b0cbec7e3c2e57d7a26178a328c7910dc6a
amalakamala <amalakamala.a@gmail.com>
2017-07-05 12:22:36 -0400

    inicio

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '236705173508168',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v2.8'
+});
+  FB.AppEvents.logPageView();   
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+
+FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+});
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
